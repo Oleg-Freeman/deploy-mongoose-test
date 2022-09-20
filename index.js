36cef5a34d100a2fbc9b2eeec1061538197dc2f7
Oleg Voitiuk <av.ssf4@gmail.com>
2022-09-20 13:47:36 +0300

    feat: dotenv configs

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-const port = 5001;
-const mongoURL =
-  "mongodb+srv://user1:qwerty123456.@cluster0.ltv3e58.mongodb.net/admin?authSource=admin&replicaSet=atlas-kety85-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true";
+const port = process.env.PORT || 5001;
+const mongoURL = process.env.MONGO_URL;
 
 app.get("/", (req, res, next) => {
   res.send("<h1>Main Page</h1>");
